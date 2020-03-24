242643a983c6736f653f9fd92a0f172252702f25
Perach Ben David <perach13@gmail.com>
2020-03-24 11:59:32 +0200

    add mongoDB support

--- server/index.js
+++ server/index.js
@@ -1,10 +1,16 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const keys = require("./config/keys");
+require("./models/user");
 require("./services/passport");
 
 const app = express();
 
 require("./routes/authRoutes")(app);
 
+mongoose.connect(keys.mongoURI);
 //  http://localhost:5000/auth/google
+//mongodb+srv://Perach:j8gAtLFxEYw1tRc1@feedbackly-iqgy9.mongodb.net/test?retryWrites=true&w=majority
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
