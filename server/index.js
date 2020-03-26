4a0be93c372545825a267c9af0a597ee6ba8da82
Perach Ben David <perach13@gmail.com>
2020-03-26 20:30:23 +0200

    adding logout option usung cookies

--- server/index.js
+++ server/index.js
@@ -1,10 +1,21 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const cookieSession = require("cookie-session");
+const passport = require("passport");
 const keys = require("./config/keys");
 require("./models/user");
 require("./services/passport");
 
 const app = express();
+
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000, //30 days in millis
+    keys: [keys.cookieKey]
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
 
 require("./routes/authRoutes")(app);
 
