18fb61fdd8d8f68495658c65f4fcc2e3cbc0e023
Perach Ben David <perach13@gmail.com>
2020-03-27 19:59:40 +0300

    separate dev and prod enviroments

--- server/index.js
+++ server/index.js
@@ -22,6 +22,8 @@ require("./routes/authRoutes")(app);
 mongoose.connect(keys.mongoURI);
 //  http://localhost:5000/auth/google
 //mongodb+srv://Perach:j8gAtLFxEYw1tRc1@feedbackly-iqgy9.mongodb.net/test?retryWrites=true&w=majority
+//prodpassword: 8nGLRuGYKtJuCJqE
+//mongodb+srv://perach:<password>@cluster0-xnlyg.mongodb.net/test?retryWrites=true&w=majority
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
