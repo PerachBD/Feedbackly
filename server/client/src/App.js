a595aea1dc90e1a161af1a0bd9f93cb89271a6e8
Perach Ben David <perach13@gmail.com>
2020-04-04 13:27:00 +0300

    init client side

--- server/client/src/App.js
+++ server/client/src/App.js
@@ -8,8 +8,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          perach
         </p>
+        <a href="/auth/google">Sign In With Google</a>
         <a
           className="App-link"
           href="https://reactjs.org"
