000cd5d9977848c4d8c32d53d18a9c7e2e5b6474
Perach Ben David <perach13@gmail.com>
2020-04-06 00:42:05 +0300

    adding redux and broserRouters

--- server/client/src/index.js
+++ server/client/src/index.js
@@ -1,17 +1,13 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import reactDom from 'react-dom';
+import {Provider} from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import App from './components/App';
+import reducers from './reducers';
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const store = createStore(reducers, {} ,applyMiddleware())
+
+reactDom.render(
+<Provider store = {store}><App/></Provider>, 
+document.querySelector('#root'));
