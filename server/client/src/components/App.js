0fc8d02416fc73a8c73389e9b97e1cd8e3c70970
Perach Ben David <perach13@gmail.com>
2020-04-10 02:05:19 +0300

    add header and basic routs to the client side

--- server/client/src/components/App.js
+++ server/client/src/components/App.js
@@ -1,25 +1,33 @@
-import React from 'react';
+import React ,{Component} from 'react';
 import { BrowserRouter, Route} from 'react-router-dom';
+import {connect} from 'react-redux';
+import * as actions from '../actions';
 
-const Header = () => <h2>Header</h2>
+import Header from './Header';
+import Landing from './Landing';
 const Dashboard = () => <h2>Dashboard</h2>
 const SurvyNew = () => <h2>SurvyNew</h2>
-const Landing = () => <h2>Landing</h2>
 
-const App = () => {
-    return (
-        <div>
-            <BrowserRouter>
-                <div>
-                    <Header />
-                    <Route exact path="/" component={Landing} />
-                    <Route exact path="/surveys" component={Dashboard} />
-                    <Route exact path="/surveys/new" component={SurvyNew} />
-                </div>
 
-            </BrowserRouter>
-        </div>
-    )
+class App extends Component {
+    componentDidMount(){
+        this.props.fetchUser();
+    }
 
+    render () {
+        return (
+            <div className="container">
+                <BrowserRouter>
+                    <div>
+                        <Header />
+                        <Route exact path="/" component={Landing} />
+                        <Route exact path="/surveys" component={Dashboard} />
+                        <Route exact path="/surveys/new" component={SurvyNew} />
+                    </div>
+
+                </BrowserRouter>
+            </div>
+        );
+    }
 }
-export default App;
+export default connect(null, actions)(App);
--- server/client/src/index.js
+++ server/client/src/index.js
@@ -1,12 +1,14 @@
+import 'materialize-css/dist/css/materialize.min.css'
 import React from 'react';
 import reactDom from 'react-dom';
 import {Provider} from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
+import reduuxThunk from 'redux-thunk';
 
 import App from './components/App';
 import reducers from './reducers';
 
-const store = createStore(reducers, {} ,applyMiddleware())
+const store = createStore(reducers, {} ,applyMiddleware(reduuxThunk))
 
 reactDom.render(
 <Provider store = {store}><App/></Provider>, 
