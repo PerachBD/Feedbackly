a9fe0f4dc455b9554e6659f36c06daa52a48b7c7
Perach Ben David <perach13@gmail.com>
2020-04-17 12:03:19 +0300

    add build heroku configuration

--- server/index.js
+++ server/index.js
@@ -21,6 +21,20 @@ app.use(passport.session());
 
 require("./routes/authRoutes")(app);
 require("./routes/billingRoutes")(app);
+
+if (process.env.NODE_ENV === 'production'){
+  // express will serve production assets
+  // like our main.js file, or main.css file
+  app.use(express.static('client/build'));
+
+  // express will serve up the index.html file
+  // if it doesn't recognize the route
+  const path =require('path');
+  app.get('*',(req,res) => {
+    res.sendFile(path.resolve(__dirname,'client','build','index.html'));
+  });
+}
+
 //npx create-react-app client
 mongoose.connect(keys.mongoURI);
 
