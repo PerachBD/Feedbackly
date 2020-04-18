a31f720829eb2fc6d6a80bfaf3658265207c1fe0
Perach Ben David <perach13@gmail.com>
2020-04-19 01:32:46 +0300

    fix heroku-postbuild

--- server/index.js
+++ server/index.js
@@ -29,9 +29,9 @@ if (process.env.NODE_ENV === 'production'){
 
   // express will serve up the index.html file
   // if it doesn't recognize the route
-  const path =require('path');
-  app.get('*',(req,res) => {
-    res.sendFile(path.resolve(__dirname,'client','build','index.html'));
+  const path =require("path");
+  app.get("*",(req,res) => {
+    res.sendFile(__dirname,"client","build","index.html");
   });
 }
 
