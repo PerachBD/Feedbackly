d2501ce2b802a19c63d813935bc38dbf18199442
Perach Ben David <perach13@gmail.com>
2020-04-23 16:53:01 +0300

    checking

--- server/index.js
+++ server/index.js
@@ -25,7 +25,7 @@ require("./routes/billingRoutes")(app);
 if (process.env.NODE_ENV === 'production'){
   // express will serve production assets
   // like our main.js file, or main.css file
-  app.use(express.static('client/build'));
+  app.use(express.static('/client/build'));
 
   // express will serve up the index.html file
   // if it doesn't recognize the route
