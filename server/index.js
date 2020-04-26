3b47d6685b485f3d189e5f8f51ce650b88d55e1b
Perach Ben David <perach13@gmail.com>
2020-04-26 16:03:28 +0300

    Survey cration

--- server/index.js
+++ server/index.js
@@ -5,6 +5,7 @@ const passport = require("passport");
 const keys = require("./config/keys");
 const bodyParser = require('body-parser');
 require("./models/User");
+require("./module/Survey")
 require("./services/passport");
 
 mongoose.connect(keys.mongoURI);
@@ -22,6 +23,7 @@ app.use(passport.session());
 
 require("./routes/authRoutes")(app);
 require("./routes/billingRoutes")(app);
+require("./routes/surveyRoutes")(app);
 
 if (process.env.NODE_ENV === 'production'){
   const path =require("path");
