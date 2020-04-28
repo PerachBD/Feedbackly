1f7fe1944fc59c35273c54bf79ea8dcf1a6b11ff
Perach Ben David <perach13@gmail.com>
2020-04-29 02:42:38 +0300

    redux form setup

--- server/client/src/components/App.js
+++ server/client/src/components/App.js
@@ -5,8 +5,8 @@ import * as actions from '../actions';
 
 import Header from './Header';
 import Landing from './Landing';
-const Dashboard = () => <h2>Dashboard</h2>
-const SurvyNew = () => <h2>SurvyNew</h2>
+import Dashboard from './Dashboard';
+import SurveyNew from './surveys/SurveyNew'
 
 
 class App extends Component {
@@ -22,7 +22,7 @@ class App extends Component {
                         <Header />
                         <Route exact path="/" component={Landing} />
                         <Route exact path="/surveys" component={Dashboard} />
-                        <Route exact path="/surveys/new" component={SurvyNew} />
+                        <Route exact path="/surveys/new" component={SurveyNew} />
                     </div>
 
                 </BrowserRouter>
