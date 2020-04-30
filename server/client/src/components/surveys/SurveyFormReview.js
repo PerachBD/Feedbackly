d374910707785757f811fd0a889261ddef9fb982
Perach Ben David <perach13@gmail.com>
2020-04-30 22:13:49 +0300

    Finalizing form review

--- server/client/src/components/surveys/SurveyFormReview.js
+++ server/client/src/components/surveys/SurveyFormReview.js
@@ -1,17 +1,44 @@
 //SurveyFormReview shows users their form inputs for review
+import _ from 'lodash';
 import React from 'react';
+import {connect} from 'react-redux';
+import formField from './formFields';
+import * as actions from "../../actions"
 
-const SurveyFormReview = ({onCancel}) =>{
+const SurveyFormReview = ({onCancel, formValues, submitSurvey}) =>{
+    const reviewFields = _.map(formField, ({name,label}) => {
+        return(
+            <div key = {name}>
+                <label>{label}</label>
+                <div>
+                    {formValues[name]}
+                </div>
+            </div>
+        );
+    });
+    
     return(
         <div>
             <h5>Please confirm your entries</h5>
+            {reviewFields}
             <button
-                className="yellow darken-3 btn-flat"
+                className="red darken-3 white-text btn-flat"
                 onClick={onCancel}
             >Back
+            </button>
+            <button 
+                onClick={() => submitSurvey(formValues)}
+                className="lime btn flat right"
+            >
+                Send Survey
+                <i className="material-icons right">email</i>
             </button>
         </div>
     );
 };
 
-export default SurveyFormReview;
+function mapStateProps(state){
+    return{ formValues: state.form.surveyForm.values };
+}
+
+export default connect(mapStateProps, actions)(SurveyFormReview);
