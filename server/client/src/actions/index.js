149f06f0cb2945fe917b39826a2215a17dfa5096
Perach Ben David <perach13@gmail.com>
2020-05-01 14:16:00 +0300

    connect survey form to emails infrastructre

--- server/client/src/actions/index.js
+++ server/client/src/actions/index.js
@@ -12,6 +12,15 @@ export const handleToken = (token)=> async dispatch=>{
     dispatch({type:FETCH_USER,payload:res});
 };
 
-export const submitSurvey = values => {
-    return{ type: 'submit_survey' }
+export const submitSurvey = (values) => async dispatch => {
+    const res = await axios.post("/api/surveys", values);
+
+    dispatch({type:FETCH_USER,payload:res});
 };
+
+// export const submitSurvey = (values, history) => async dispatch => {
+//     const res = await axios.post('/api/surveys', values);
+  
+//     history.push('/surveys');
+//     dispatch({ type: FETCH_USER, payload: res.data });
+//   };
--- server/routes/surveyRoutes.js
+++ server/routes/surveyRoutes.js
@@ -14,11 +14,13 @@ module.exports = app => {
     app.post('/api/surveys', requireLogin, requireCredits, async (req,res) => {
         const {title,subject,body,recipients } = req.body;
 
-        const survey = new Survey({
+        const survey = new Survey({ 
             title,
             subject,
             body,
-            recipients: recipients.split(',').map(email => ({ email: email.trim() })),
+            recipients: recipients
+            .split(',')
+            .map(email => ({ email: email.trim() })),
             _user: req.user.id,
             dateSent:Date.now()
         });
